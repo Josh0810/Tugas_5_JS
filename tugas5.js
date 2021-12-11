c5d483265744b90dbeead40699581831e736158d
Josh0810 <92926113+Josh0810@users.noreply.github.com>
2021-12-12 04:34:38 +0700

    Add files via upload

---
+++ tugas5.js
@@ -0,0 +1,18 @@
+for (var a = 0; a <= 20; a++) {
+  if (a == 0) continue;
+  if (a == 1) continue;
+  if (a == 2) continue;
+  if (a == 4) continue;
+  if (a == 5) continue;
+  if (a == 7) continue;
+  if (a == 8) continue;
+  if (a == 10) continue;
+  if (a == 11) continue;
+  if (a == 13) continue;
+  if (a == 14) continue;
+  if (a == 16) continue;
+  if (a == 17) continue;
+  if (a == 19) continue;
+  if (a == 20) continue;
+  console.log(a);
+}
